28050b9181e194371dd72b1b82efa21fc2e28721
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-06 07:51:55 -0800

    hardcoded data insert successfully

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// username: dbuser3
+// pass : UuOMZVpG2Co38wmT
+const uri = "mongodb+srv://dbuser3:UuOMZVpG2Co38wmT@cluster0.ltjdg3f.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+
+        const userCollection = client.db('nodeMongoCurdPractice').collection('users');
+        const user = {
+            name: 'testitng practice 2',
+            email: 'testingpractice2@gmail.com'
+        }
+        const result = await userCollection.insertOne(user);
+        console.log(result);
+
+    }
+
+    finally{
+
+    }
+
+}
+
+run().catch(err => console.log(err));
+
+
+
+
 
 app.get('/', (req, res) =>{
     res.send('Hello from node mongo crud server');
