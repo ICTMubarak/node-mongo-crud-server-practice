de99f239634b58098c76338a82085d7dd6cddac8
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-07 04:24:07 -0800

    Delete operation complited

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,13 +20,33 @@ async function run(){
     try{
 
         const userCollection = client.db('nodeMongoCurdPractice').collection('users');
+
+        app.get('/users', async(req, res)=>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+
+        });
+
+
         app.post('/users', async(req, res) =>{
             const user = req.body;
             console.log(user); 
 
             const result = await userCollection.insertOne(user);
             res.send(result);
-        })
+        });
+
+        app.delete('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result =await userCollection.deleteOne(query);
+            console.log('trying to delete', result);
+            res.send(result);
+
+
+        });
 
     }
 
